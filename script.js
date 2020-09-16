183bb47b1382773b91724ce7770e7c77997a8953
mercspring <68706508+mercspring@users.noreply.github.com>
2020-09-16 09:56:10 -0700

    added visual hours to the slots, changed code to take advantage of prewritten css, now changes current hour to red

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ console.log(moment().format('LT'))
 $("#currentDay").text(moment().format('ll'))
 
 var timeSlots = [8,9,10,11,12,13,14,15,16,17,18];
+var timeSlots = [{militaryTime: 8, standardTime: "8am"},
+{militaryTime: 9, standardTime: "9am"},
+{militaryTime: 10, standardTime: "10am"},
+{militaryTime: 11, standardTime: "11am"},
+{militaryTime: 12, standardTime: "12pm"},
+{militaryTime: 13, standardTime: "1pm"},
+{militaryTime: 14, standardTime: "2pm"},
+{militaryTime: 15, standardTime: "3pm"},
+{militaryTime: 16, standardTime: "4pm"},
+{militaryTime: 17, standardTime: "5pm"},
+{militaryTime: 18, standardTime: "6pm"},];
 timeSlots.forEach(color)
 function color(hourBlock){
 // console.log(currentHourBlock, hourBlock)
@@ -18,14 +29,23 @@ mainCol = $('<div class="col-10">');
 saveCol = $('<div class="col-1">');
 hourCol = $('<div class="col-1">');
 textArea = $('<textarea>');
-if(hourBlock < currentHourBlock){
-    textArea.css("background-color", "gray")
+hourCol.text(hourBlock.standardTime);
+hourCol.addClass("hour");
+// Change time blocks based on time of day
+if(hourBlock.militaryTime < currentHourBlock){
+    textArea.addClass("past")
+}else if(currentHourBlock === hourBlock.militaryTime) {
+
+    textArea.addClass("present")
+} else{
+
+    textArea.addClass("future")
 }
+
 row.append(hourCol)
 row.append(mainCol)
 row.append(saveCol)
 mainCol.append(textArea)
 $('.container').append(row);
 })
-// Change time blocks based on time of day
 console.log(timeSlots)
